588306cbb9e2331408ce06e80bae8953eee8670b
Yeoh Ming Zhe <152019114+mingzheYeoh@users.noreply.github.com>
2024-03-19 23:20:00 +0800

    update P4

--- FourTierApp/src/control/MaintainProgrammeControl.java
+++ FourTierApp/src/control/MaintainProgrammeControl.java
@@ -1,18 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package control;
 
 import da.ProgrammeDA;
 import domain.Programme;
 
 public class MaintainProgrammeControl {
-
+    
     private ProgrammeDA progDA;
-
-    public MaintainProgrammeControl() {
+    
+    public MaintainProgrammeControl(){
         progDA = new ProgrammeDA();
     }
-
-    public Programme selectRecord(String code) {
+    
+    public Programme selectRecord(String code){
         return progDA.getRecord(code);
     }
-
+    
+    public void addRecord(Programme programme){
+        progDA.addRecord(programme);
+    }
+    
+    public void updateRecord(Programme programme){
+        progDA.updateRecord(programme);
+    }
+    
+    public void deleteRecord(Programme programme){
+        progDA.deleteRecord(programme);
+    }
 }
--- FourTierApp/src/ui/MaintainProgrammeFrame.java
+++ FourTierApp/src/ui/MaintainProgrammeFrame.java
@@ -9,9 +9,11 @@ import javax.swing.*;
 public class MaintainProgrammeFrame extends JFrame {
 
     private MaintainProgrammeControl progControl;
+
     private JTextField jtfCode = new JTextField();
     private JTextField jtfName = new JTextField();
     private JTextField jtfFaculty = new JTextField();
+
     private JButton jbtAdd = new JButton("Create");
     private JButton jbtRetrieve = new JButton("Retrieve");
     private JButton jbtUpdate = new JButton("Update");
@@ -19,7 +21,7 @@ public class MaintainProgrammeFrame extends JFrame {
 
     public MaintainProgrammeFrame() {
         progControl = new MaintainProgrammeControl();
-        
+
         JPanel jpCenter = new JPanel(new GridLayout(3, 2));
         jpCenter.add(new JLabel("Programme Code"));
         jpCenter.add(jtfCode);
@@ -35,10 +37,36 @@ public class MaintainProgrammeFrame extends JFrame {
         jpSouth.add(jbtUpdate);
         jpSouth.add(jbtDelete);
         add(jpSouth, BorderLayout.SOUTH);
-
-        jbtRetrieve.addActionListener(new RetrieveListener());
-
         
+        setTitle("Programme CRUD");
+        setSize(600, 200);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+
+        jbtAdd.addActionListener(new AddListener());
+        jbtRetrieve.addActionListener(new RetrieveListener());
+        jbtUpdate.addActionListener(new UpdateListener());
+        jbtDelete.addActionListener(new DeleteListener());
+    }
+
+    private class AddListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            String code = jtfCode.getText();
+            Programme p = progControl.selectRecord(code);
+
+            if (p != null) {
+                JOptionPane.showMessageDialog(null, "This programme code already exist.", "DUPLICATE RECORD", JOptionPane.ERROR_MESSAGE);
+            } else {
+                p = new Programme(code, jtfName.getText(), jtfFaculty.getText());
+                progControl.addRecord(p);
+                JOptionPane.showMessageDialog(null, "New Programme added.");
+                clearText();
+            }
+        }
+
     }
 
     private class RetrieveListener implements ActionListener {
@@ -46,22 +74,68 @@ public class MaintainProgrammeFrame extends JFrame {
         @Override
         public void actionPerformed(ActionEvent e) {
             String code = jtfCode.getText();
-            Programme programme = progControl.selectRecord(code);
-            if (programme != null) {
-                jtfName.setText(programme.getName());
-                jtfFaculty.setText(programme.getFaculty());
+            Programme p = progControl.selectRecord(code);
+            if (p != null) {
+                jtfName.setText(p.getName());
+                jtfFaculty.setText(p.getFaculty());
             } else {
                 JOptionPane.showMessageDialog(null, "No such programme code.", "RECORD NOT FOUND", JOptionPane.ERROR_MESSAGE);
             }
         }
     }
 
+    private class UpdateListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            String code = jtfCode.getText();
+            Programme p = progControl.selectRecord(code);
+            
+            if (p != null) {
+                p.setName(jtfName.getText());
+                p.setFaculty(jtfFaculty.getText());
+
+                progControl.updateRecord(p);
+                JOptionPane.showMessageDialog(null, "Record Updated.");
+                clearText();
+            } else {
+                JOptionPane.showMessageDialog(null, "No such programme code.", "RECORD NOT FOUND", JOptionPane.ERROR_MESSAGE);
+            }
+        }
+    }
+
+    private class DeleteListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            String code = jtfCode.getText();
+            Programme p = progControl.selectRecord(code);
+            
+            if (p != null) {
+                jtfName.setText(p.getName());
+                jtfFaculty.setText(p.getFaculty());
+                int option = JOptionPane.showConfirmDialog(null, "Are You Sure?");
+
+                if (option == JOptionPane.YES_OPTION) {
+                    progControl.deleteRecord(p);
+                    JOptionPane.showMessageDialog(null, "Record Deleted.");
+                    clearText();
+                }
+            } else {
+                JOptionPane.showMessageDialog(null, "Record does not exist.");
+            }
+        }
+    }
+
+    private void clearText() {
+        jtfCode.setText("");
+        jtfName.setText("");
+        jtfFaculty.setText("");
+        jtfCode.requestFocusInWindow();
+    }
+
     public static void main(String[] args) {
-          MaintainProgrammeFrame frm=new MaintainProgrammeFrame();   
-          frm.setTitle("Programme CRUD");
-          frm.setSize(600, 200);
-          frm.setLocationRelativeTo(null);
-          frm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-          frm.setVisible(true);
+        new MaintainProgrammeFrame();
+        
     }
 }
