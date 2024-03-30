04198632701510a2efac085469561d7a0044056e
Yeoh Ming Zhe <152019114+mingzheYeoh@users.noreply.github.com>
2024-03-30 10:15:11 +0800

    P6

--- P6/src/java/servlets/ComputeLoanPayment.java
+++ P6/src/java/servlets/ComputeLoanPayment.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
- */
 package servlets;
 
 import domain.Loan;
@@ -9,6 +5,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -22,26 +19,31 @@ public class ComputeLoanPayment extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html");
         PrintWriter out = resp.getWriter();
-        
+
         String amtStr = req.getParameter("amount");
         String rateStr = req.getParameter("rate");
         String durationStr = req.getParameter("duration");
-        
-        if (amtStr.length() == 0 || rateStr.length() == 0 || durationStr.length() == 0){
+
+        if (amtStr.length() == 0 || rateStr.length() == 0 || durationStr.length() == 0) {
             out.println("Require all infos");
-        }else{
+        } else {
             double amt = Double.parseDouble(amtStr);
             double rate = Double.parseDouble(rateStr);
             int duration = Integer.parseInt(durationStr);
-            
+
             Loan loan = new Loan(rate, duration, amt);
             
+            //Get a session or create one if it does not exist
+            HttpSession httpSession = req.getSession();
+        
+            httpSession.setAttribute("loan", loan);
+
             //Ask for information
             out.println("You entered the following data:");
             out.println("<p>Loan Amount: RM" + String.format("%.2f", amt) + "</p>");
             out.println("<br> Annual interest rate: " + String.format("%.2f", rate));
             out.println("<br> Number of years: " + duration);
-            
+
             //Set the action for processing
             out.println("<p><form method=\"post\" action=\"ComputeLoanPayment\"></p>");
             out.println("<p><input type=\"submit\" value=\"Confirm\"></p>");
@@ -50,5 +52,25 @@ public class ComputeLoanPayment extends HttpServlet {
         }
     }
 
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        
+        //Obtain the session
+        HttpSession httpSession = req.getSession();
+        
+        //Get the Loan from httpSession
+        Loan loan = (Loan) (httpSession.getAttribute("loan"));
+        
+        out.println("Loan Amount: RM" + String.format("%.2f", loan.getLoanAmount()));
+        out.println("<br> Annual interest rate: " + String.format("%.2f", loan.getAnnualInterestRate()));
+        out.println("<br> Number of Years: " + loan.getNumberOfYears());
+        out.println("<br> Monthly Payment: RM" + String.format("%.2f", loan.getMonthlyPayment()));
+        out.println("<br> Total Payment: RM" + String.format("%.2f", loan.getTotalPayment()));
+        out.close();
+        
+    }
+    
     
 }
